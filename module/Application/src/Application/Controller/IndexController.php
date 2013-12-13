1a90afcab74698e6c7c804f6c93083639e28b8f5
Santosh Moktan <santosh@amplecube.com>
2013-12-13 15:36:12 +0545

    initialize zend with doctrine, zfuser, zfuserdoctrineorm

---
+++ module/Application/src/Application/Controller/IndexController.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function doctrineAction()
+    {
+        $objectManager = $this
+                ->getServiceLocator()
+                ->get('Doctrine\ORM\EntityManager');
+
+        $user = new \Application\Entity\User();
+        $user->setFullName('Marco Pivetta');
+
+        $objectManager->persist($user);
+        $objectManager->flush();
+
+        die(var_dump($user->getId())); // yes, I'm lazy
+    }
+
+}
